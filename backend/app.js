9e7824a4142b2ec0bcf87e906e7fc5f506ea18ea
Fabian Waltermann <waltermann.fabian@fh-swf.de>
2022-06-07 12:11:11 +0200

    Renaming of Directories from static to frontend and js to backend

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express'); // express framework
 const cors = require('cors'); // for cross-origin resource sharingq
 const api = require('./api.js'); // our api routes
 const app = express();
-app.use(express.static('static')); //static directory
+app.use(express.static('frontend')); //static directory
 app.use(cors());
 app.use(express.json());  // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
